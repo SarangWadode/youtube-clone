9571d2b48791d9218034b34e360491287bd6d7e1
Sarang Wadode <sarangwadode@gmail.com>
2021-12-25 10:09:08 +0530

    other icons added to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,10 @@ import ExploreIcon from '@mui/icons-material/Explore';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
 import RestoreIcon from '@mui/icons-material/Restore';
+import PersonalVideoIcon from '@mui/icons-material/PersonalVideo';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export default function Sidebar() {
     return (
@@ -14,8 +18,14 @@ export default function Sidebar() {
                 <SidebarRows selected Icon={HomeIcon} title='Home' />
                 <SidebarRows Icon={ExploreIcon} title='Explore' />
                 <SidebarRows Icon={SubscriptionsIcon} title='Subscription' />
+                <hr />
                 <SidebarRows Icon={VideoLibraryIcon} title='Library' />
                 <SidebarRows Icon={RestoreIcon} title='History' />
+                <SidebarRows Icon={PersonalVideoIcon} title='Your videos' />
+                <SidebarRows Icon={WatchLaterIcon} title='Watch later' />
+                <SidebarRows Icon={ThumbUpIcon} title='Liked video' />
+                <SidebarRows Icon={KeyboardArrowDownIcon} title='Show more' />
+                <hr />
             </div>
         </div>
     )
